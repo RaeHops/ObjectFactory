d5167ed80feabfdec92d0b722fc4b5a125cc7a75
RaeHops <raegan_hopkins26@milton.edu>
2023-10-25 09:01:59 -0400

    Made default pony and made an objecct of pony.

---
+++ src/Ponies.java
@@ -0,0 +1,12 @@
+public class Ponies {
+    //true/false -> boolean
+    //numbers -> int, double int
+    //words -> Strings
+
+    boolean isUnicorn = false;
+    boolean isPegasus = false;
+
+    double size = 20.56;
+    String name = "bobby";
+    String color = "pink";
+}
