deb7afb05c49c1aa875aca8b278db1fe6cea14ae
RaeHops <Raegan_hopkins26@milton.edu>
2023-10-31 13:08:59 -0400

    Made a new pony and described a pony

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Object Factory!");
         Ponies p = new Ponies();
         System.out.println(p.isUnicorn);
         p.isUnicorn = true;
         System.out.println(p.isUnicorn);
+        System.out.println(p.name);
 
+        p.describePony();
 
+        Ponies a;
+        a = new Ponies();
+        a.name = "Apple Jack";
+        System.out.println("pony a's name is" + a.name);
     }
 }
--- src/Ponies.java
+++ src/Ponies.java
@@ -7,6 +7,10 @@ public class Ponies {
     boolean isPegasus = false;
 
     double size = 20.56;
-    String name = "bobby";
+    String name = "Pinkie Pie";
     String color = "pink";
+
+    public void describePony(){
+        System.out.println("this is pony " + name + ". they are the color " + color + ". The size of " +name+ " is " + size + " inches. it is " +isUnicorn+ " that the pony is a unicorn. it is "+isPegasus+" that the pony is a pegasus");
+    }
 }
