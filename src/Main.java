4b2b6e5f1f119baa9c33a703c583ad407b6444cf
RaeHops <Raegan_hopkins26@milton.edu>
2023-11-02 09:39:11 -0400

    Made new ponies and worked with constructors

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Object Factory!");
-        Ponies p = new Ponies();
+        Ponies p = new Ponies("Pinky Pie", "pink", 20.56, false, true);
         System.out.println(p.isUnicorn);
         p.isUnicorn = true;
         System.out.println(p.isUnicorn);
@@ -10,8 +10,19 @@ public class Main {
         p.describePony();
 
         Ponies a;
-        a = new Ponies();
-        a.name = "Apple Jack";
-        System.out.println("pony a's name is" + a.name);
+        a = new Ponies("Apple Jack", "orange", 19.89, true, false); //this "Ponies" is a constructor, there is no "Ponies" method in ponies.java so it is a constructor method.
+        // a.name = "Apple Jack";
+        System.out.println("pony a's name is " + a.name);
+        a.describePony();
+
+        Ponies f;
+        f = new Ponies("Fluttershy", "yellow", 17.38, true, true);
+        // f.name = "Fluttershy";
+        //f.color = "yellow";
+        //f.size = 17.38;
+        //f.isUnicorn = false;
+        //f.isPegasus = true;
+
+        f.describePony();
     }
 }
--- src/Ponies.java
+++ src/Ponies.java
@@ -3,12 +3,26 @@ public class Ponies {
     //numbers -> int, double int
     //words -> Strings
 
-    boolean isUnicorn = false;
-    boolean isPegasus = false;
+    boolean isUnicorn;
+    boolean isPegasus;
 
-    double size = 20.56;
-    String name = "Pinkie Pie";
-    String color = "pink";
+    double size;
+    String name;
+    String color;
+
+    public Ponies(String pName, String pColor, double pSize, boolean pIsPegasus, boolean pIsUnicorn){
+        //Constructors are special methods
+        //Constructors dont have "void" like other methods
+        //Constructors need to have the same exact name as the class.
+        System.out.println("Made a new pony!");
+        isUnicorn = pIsUnicorn;
+        isPegasus = pIsPegasus;
+        size = pSize;
+        name = pName;
+        color = pColor;
+
+
+    }
 
     public void describePony(){
         System.out.println("this is pony " + name + ". they are the color " + color + ". The size of " +name+ " is " + size + " inches. it is " +isUnicorn+ " that the pony is a unicorn. it is "+isPegasus+" that the pony is a pegasus");
